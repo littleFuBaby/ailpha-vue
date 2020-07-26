9cb976a77c40e6e97e800c061a7cdfb6bf530957
andy fu <520fys008@163.com>
2020-07-26 08:03:49 +0800

    power by using java api

---
+++ 50/PowByApi.java
@@ -0,0 +1,17 @@
+package com.ruoyi.grokking.algorithms.number;
+
+public class PowByApi {
+
+    /**
+     * method 1<br/>
+     * note: directly use java api to calculate number power
+     *
+     * @param x
+     * @param n
+     * @return
+     */
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+
+}
