20cf3c98c15e5120438d284604bc54411d5b195c
andy <yunsonglife@sina.com>
2023-01-17 21:23:47 +0800

    delete all files

--- Offer 05/ReplaceSpace.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.grokking.algorithms.string;
-
-public class ReplaceSpace {
-
-    public String replaceSpace(String s) {
-        if (s == null) return null;
-        return s.replace(" ", "%20");
-    }
-}
--- Offer 05/ReplaceSpaceAll.java
+++
@@ -1,9 +0,0 @@
-package com.ruoyi.grokking.algorithms.string;
-
-public class ReplaceSpaceAll {
-
-    public String replaceSpace(String s) {
-        if (s == null) return null;
-        return s.replaceAll(" ", "%20");
-    }
-}
