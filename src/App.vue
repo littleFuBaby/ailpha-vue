cbe9056638f3d7846d399ce04d7fd9a2e15b668b
andy <yunsonglife@sina.com>
2023-05-25 21:19:43 +0800

    update file

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,36 @@ import TheWelcome from './components/TheWelcome.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
+    <div>
+      <button @click="getData">获取数据</button>
+      <ul>
+        <li v-for="item in list" :key="item.id">{{ item.name }}</li>
+      </ul>
+    </div>
   </header>
 
   <main>
     <TheWelcome />
   </main>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    async getData () {
+      //const response = await axios.get('/api/data')
+      //this.list = response.data
+      this.list = '1345345';
+    }
+  }
+}
+</script>
 
 <style scoped>
 header {
